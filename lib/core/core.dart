6635ca337c77c90d0b02bc815fe593d3a8f9dbc9
alemoles <alemolerio@gmail.com>
2025-09-27 22:20:43 -0300

    Tooling core and shared modules

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,8 +1,13 @@
+// Models
 export 'models/campaign.dart';
 export 'models/gps_point.dart';
 export 'models/route.dart';
 export 'models/user.dart';
 
 
+// Utils
 export 'utils/permission_helper.dart';
 export 'utils/image_helper.dart';
+
+// Constants
+export 'constants/colors.dart';
---
+++ lib/shared/shared.dart
@@ -0,0 +1,8 @@
+// Repositories
+export 'repositories/campaign_repository.dart';
+export 'repositories/gps_repository.dart';
+export 'repositories/auth_repository.dart';
+
+// Services
+export 'services/connectivity_service.dart';
+export 'services/sync_service.dart';
